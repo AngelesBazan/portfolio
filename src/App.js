9856b4fcc8924be7bbad7b9216d5e14ddf90f9c9
Angy Bazan <angeles_1336@hotmail.com>
2022-08-06 16:50:11 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      Angeles Bazan
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/SobreMi" element={<AboutMe/>} />
+        <Route path="/Portfolio" element={<Portfolio/>} />
+        <Route path="/Contacto" element={<Contact/>} />
+      </Routes>
+    </Router>
   )
 }
 
