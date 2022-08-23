b3da68a7cb4778e0b51723356005f6499b1b8108
Angy Bazan <angeles_1336@hotmail.com>
2022-08-23 13:14:21 -0300

    add docs

---
+++ docs/precache-manifest.9873a8fed65020654ab20b3949f54a70.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d8cb30103cb2c565aec",
+    "url": "/static/css/main.be76ad6e.chunk.css"
+  },
+  {
+    "revision": "2d8cb30103cb2c565aec",
+    "url": "/static/js/main.2d8cb301.chunk.js"
+  },
+  {
+    "revision": "1cfb574cb02c33354c01",
+    "url": "/static/css/1.84f20ead.chunk.css"
+  },
+  {
+    "revision": "1cfb574cb02c33354c01",
+    "url": "/static/js/1.1cfb574c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7608962e519199d3b0f96bdcfee4cd58",
+    "url": "/static/media/slide-1.7608962e.jpg"
+  },
+  {
+    "revision": "dcbf0393d0ddda25fd013fe9fa3b2be5",
+    "url": "/static/media/slide-2.dcbf0393.jpg"
+  },
+  {
+    "revision": "4f30a118df29159028ecf09ca6ecc015",
+    "url": "/static/media/slide-3.4f30a118.jpg"
+  },
+  {
+    "revision": "5a623d1f11ea133597187b9e63281e6b",
+    "url": "/static/media/perfil.5a623d1f.jpeg"
+  },
+  {
+    "revision": "1e0f9bf852021398b80a5f65e701d0ea",
+    "url": "/static/media/home-bg.1e0f9bf8.jpeg"
+  },
+  {
+    "revision": "08a785aaf8faa9114cd21e84f360e58f",
+    "url": "/index.html"
+  }
+];
