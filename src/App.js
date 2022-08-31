33b3b1357bacb4df53816756981b3f6e976f7149
Angy Bazan <angeles_1336@hotmail.com>
2022-08-31 15:44:14 -0300

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Portafolio from "./pages/Portafolio";
@@ -8,14 +8,12 @@ import "./App.css";
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/SobreMi" element={<AboutMe/>} />
-        <Route path="/Portafolio" element={<Portafolio/>} />
-        <Route path="/Contacto" element={<Contact/>} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/SobreMi" element={<AboutMe/>} />
+      <Route path="/Portafolio" element={<Portafolio/>} />
+      <Route path="/Contacto" element={<Contact/>} />
+    </Routes>
   )
 }
 
