11f668bb16d4fda345c98c8a07bb55218a616c0d
Angy Bazan <angeles_1336@hotmail.com>
2022-08-31 15:16:43 -0300

    change routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Portafolio from "./pages/Portafolio";
@@ -8,14 +8,14 @@ import "./App.css";
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/SobreMi" element={<AboutMe/>} />
         <Route path="/Portafolio" element={<Portafolio/>} />
         <Route path="/Contacto" element={<Contact/>} />
       </Routes>
-    </Router>
+    </HashRouter>
   )
 }
 
