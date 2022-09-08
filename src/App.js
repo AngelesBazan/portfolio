b6da330594e521bb6e2e75f067758bdc5fd17d15
Angy Bazan <angeles_1336@hotmail.com>
2022-09-08 16:00:17 -0300

    fix index.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Portafolio from "./pages/Portafolio";
@@ -8,12 +8,14 @@ import "./App.css";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/SobreMi" element={<AboutMe/>} />
-      <Route path="/Portafolio" element={<Portafolio/>} />
-      <Route path="/Contacto" element={<Contact/>} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/SobreMi" element={<AboutMe/>} />
+        <Route path="/Portafolio" element={<Portafolio/>} />
+        <Route path="/Contacto" element={<Contact/>} />
+      </Routes>
+    </Router>
   )
 }
 
